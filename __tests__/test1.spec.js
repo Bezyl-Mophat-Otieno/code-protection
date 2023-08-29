081b19d3ae5024fb9d2277729f9408cb371464e1
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-08-29 12:49:56 +0300

    Added the tests and the workflow yml file

---
+++ __tests__/test1.spec.js
@@ -0,0 +1,5 @@
+describe("Math Test", () => {
+  it("it should pass when the sum equals 2", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
---
+++ __tests__/test3.spec.js
@@ -0,0 +1,5 @@
+describe("Math Test", () => {
+  it("it should pass when the sum equals 2", () => {
+    expect(4 / 2).toBe(2);
+  });
+});
