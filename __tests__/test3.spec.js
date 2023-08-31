5d41601323f07fdcf518a8429ae9cb26ef7d0c61
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-08-31 06:53:54 +0300

    Added some more  comments

--- __tests__/test3.spec.js
+++ __tests__/test3.spec.js
@@ -1,3 +1,4 @@
+// Jest will look for test files with any of the following popular naming conventions:
 describe("Math Test", () => {
   it("it should pass when the sum equals 2", () => {
     expect(8 / 2).toBe(4);
